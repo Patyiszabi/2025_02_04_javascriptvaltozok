45ae5c1a740972cae6b2c3910eae336f902245b2
KardosLevente <kardos.levente@diak.szbi-pg.hu>
2025-02-04 10:32:47 +0100

    Példák változókra

---
+++ 01_valtozok.js
@@ -0,0 +1,15 @@
+var a = 2;
+console.log(a);
+
+let b = 3;
+b = 4;
+console.log(b);
+const c = 5;
+
+var x = 5;
+console.log(x);
+var x = "alma";
+console.log(x);
+
+let d = 3;
+console.log(d);
---
+++ 02_valtozo_tipusok.js
@@ -0,0 +1,42 @@
+let e = 2;
+console.log('e =' + e + ' tipusa:' + typeof e);
+const pi = 3.14;
+console.log('pi =' + pi + ' tipusa:' + typeof pi);
+
+console.log(0.1+0.2);
+console.log((0.1+0.2).toFixed(2));
+
+console.log(1/0)
+
+// String
+let firstName = "John";
+let lastName = "Doe";
+// template literal (Template strings)
+// backtick (`)
+console.log(`Hello, ${firstName} ${lastName}!`);  
+
+// Object
+let person = 
+{
+    name: "Pagare",
+    age : 20,
+    isStudent: false,
+    
+}
+
+console.log(person);
+console.log(person.name)
+console.log(person.age)
+console.log(person.isStudent)
+console.log(person.address) // undefined
+
+let xy;
+console.log(xy);
+
+//boolean
+console.log("Boolean")
+console.log(typeof true);
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean("a"));
+console.log(Boolean(1));
